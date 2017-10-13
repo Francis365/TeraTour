7deded291321409cb21e15d038a16ee88f65f742
francis365 <anyeji.francis@gmail.com>
2017-10-13 04:34:33 +0100

    Updated marker_info_layout

--- app/src/main/java/com/pikkart/trial/teratour/ImageCloudRecoClass.java
+++ app/src/main/java/com/pikkart/trial/teratour/ImageCloudRecoClass.java
@@ -131,7 +131,7 @@ public class ImageCloudRecoClass extends AppCompatActivity implements IRecogniti
 
         cameraTopLayer.addView(toolbar);
 
-        //menu layout hjkhv
+        //menu layout
 
 
         /*NavigationView drawerLayout = (NavigationView) inflater.inflate(R.layout.menu, null);
